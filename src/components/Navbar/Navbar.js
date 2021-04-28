40a978cf70265749169f1f717ea0dcfe1835aeaa
vvvbb <vb@vvbb>
2021-04-28 19:22:56 +0200

    TourList Tour created

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,28 @@
-import React from 'react'
-import logo from '../../512-300x200.jpg';
-import './navbar.scss'
+import React from "react";
+import "./navbar.scss";
 export default function Navbar() {
-    return (
-       <nav className="navbar">
-           <img src={logo} alt="city tours guide"/>
-           <ul className="nav-links">
-               <li>
-                   <a href="/" className="nav-link">home</a>
-               </li>
-               <li>
-                   <a href="/" className="nav-link">about</a>
-               </li>
-               <li>
-                   <a href="/" className="nav-link active">tours</a>
-               </li>
-           </ul>
+  const logo = "https://picsum.photos/250/80";
 
-       </nav>
-    )
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="city tours guide" />
+      <ul className="nav-links">
+        <li>
+          <a href="/" className="nav-link">
+            home
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">
+            about
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link active">
+            tours
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
---
+++ src/components/TourList/TourList.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Tour from "../Tour/Tour";
+import "./tourlist.scss";
+import { tourData } from "../../tourData";
+
+export default class TourList extends Component {
+  state = {
+    tours: tourData,
+  };
+
+  removeTour = (id) => {
+    console.log(id);
+  };
+
+  render() {
+    // console.log(this.state.tours);
+    const { tours } = this.state; //destructur
+
+    return (
+      <section className="tourlist">
+        {tours.map((tour) => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+        ))}
+      </section>
+    );
+  }
+}
